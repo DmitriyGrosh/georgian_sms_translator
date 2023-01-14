4a3b7bb9889fbc227e7cb4ad30e57a4c1e785595
Dmitriy Grosh <d.grosh@wis.software>
2023-01-14 22:48:35 +0300

    add settings switcher

--- pages/settings/Settings.style.ts
+++ pages/settings/Settings.style.ts
@@ -5,5 +5,17 @@ export const settingsStyle = StyleSheet.create({
 		display: 'flex',
 		flexDirection: 'column',
 		height: '100%',
+		paddingLeft: 10,
+		paddingRight: 10,
+		paddingTop: 10
 	},
+	themeContext: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	themeText: {
+		fontSize: 20
+	}
 });
